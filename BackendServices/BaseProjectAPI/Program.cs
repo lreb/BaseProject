9f7684ea6309ef3fcca592be12ae0572ac4ec2c5
Luis Raul Espinoza Barboza <respinozabarboza@outlook.com>
2021-06-15 17:32:43 -0600

    add serilog feature logger

--- BackendServices/BaseProjectAPI/Program.cs
+++ BackendServices/BaseProjectAPI/Program.cs
@@ -1,6 +1,10 @@
 using BaseProjectAPI.Persistence.Seeds;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace BaseProjectAPI
 {
@@ -8,14 +12,40 @@ namespace BaseProjectAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .MigrateDatabase()
-                .Run();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("BaseProject - Luis Raul Espinoza Barboza");
+                Log.Information("Starting up");
+                CreateHostBuilder(args)
+                    .Build()
+                    .MigrateDatabase()
+                    .Run();
+                
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally 
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
