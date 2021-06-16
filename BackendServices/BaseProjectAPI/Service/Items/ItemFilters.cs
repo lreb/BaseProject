8d692237ab210247f0c540ffd58fec883af3dc22
Luis Raul Espinoza Barboza <respinozabarboza@outlook.com>
2021-06-16 15:44:14 -0600

    Added comments

--- BackendServices/BaseProjectAPI/Service/Items/ItemFilters.cs
+++ BackendServices/BaseProjectAPI/Service/Items/ItemFilters.cs
@@ -7,13 +7,27 @@ namespace BaseProjectAPI.Service.Items
 {
     public static class ItemFilters
     {
+        /// <summary>
+        /// Retrieve most recent items
+        /// </summary>
+        /// <param name="query"><see cref="Item"/></param>
+        /// <param name="yearsAgo"><see cref="int"/></param>
+        /// <returns><see cref="IQueryable"/></returns>
         public static IQueryable<Item> SpecificYearsAgo(this IQueryable<Item> query, int yearsAgo)
         {
             return query.Where(c => c.CreatedOn > DateTime.Now.AddYears(-yearsAgo));
         }
 
+        /// <summary>
+        /// Evaluate Items with specific date
+        /// </summary>
         private static Expression<Func<Item, int, bool>> SpecificYearsAgoExpression { get; } = (item, years) => item.CreatedOn > DateTime.Now.AddYears(-years);
 
+        /// <summary>
+        /// Retrieves enabled items
+        /// </summary>
+        /// <param name="items"><see cref="Item"/></param>
+        /// <returns><see cref="IQueryable"/></returns>
         public static IQueryable<Item> EnabledItems(this IQueryable<Item> items)
         {
             return items.Where(IsEnabledExpression);
